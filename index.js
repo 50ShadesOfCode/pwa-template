2cbaaa7735752074dc4a5f2d70e39e50cac07be4
50ShadesOfCode <48156891+50ShadesOfCode@users.noreply.github.com>
2020-07-16 16:50:19 +0300

    Code structurizing

--- index.js
+++ index.js
@@ -1,33 +1,17 @@
-/*var installPromptEvent;
-var btnInstall = document.querySelector('#install');
-
-window.addEventListener('beforeinstallprompt', function (event) {
-    event.preventDefault();
-    installPromptEvent = event;
-    btnInstall.removeAttribute('disabled');
-});
-
-btnInstall.addEventListener('click', function () {
-    btnInstall.setAttribute('disabled', '');
-    installPromptEvent.prompt();
-    installPromptEvent.userChoise.then((choice) => {
-        if (choice.outcome == "accepted"){
-            console.log('User accepted the A2HS prompt');
-        }else{
-            console.log('User dismissed the A2HS prompt');
-        }
-        installPromptEvent = null;
-    });
-});*/
 var installPromptEvent,
     btnInstall = document.getElementById("install")
 
-window.addEventListener("beforeinstallprompt", function (t) {
-    t.preventDefault(), installPromptEvent = t, btnInstall.removeAttribute("disabled")
+window.addEventListener("beforeinstallprompt", function (event) {
+    event.preventDefault();
+    installPromptEvent = event;
+    btnInstall.removeAttribute("disabled");
 });
 
 btnInstall.addEventListener("click", function () {
-    btnInstall.setAttribute("disabled", ""), installPromptEvent.prompt(), installPromptEvent.userChoice.then(function (t) {
-        "accepted" === t.outcome ? console.log("User accepted the A2HS prompt") : console.log("User dismissed the A2HS prompt"), installPromptEvent = null
+    btnInstall.setAttribute("disabled", "");
+    installPromptEvent.prompt();
+    installPromptEvent.userChoice.then(function (t) {
+        "accepted" === t.outcome ? console.log("User accepted the A2HS prompt") : console.log("User dismissed the A2HS prompt");
+    installPromptEvent = null;
     })
 });
