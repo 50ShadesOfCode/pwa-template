6c712bdecf3e65f292981aaa2f46e102b390d068
50ShadesOfCode <48156891+50ShadesOfCode@users.noreply.github.com>
2020-07-17 14:41:24 +0300

    Code structuring

--- sw.js
+++ sw.js
@@ -4,14 +4,14 @@ const cacheName = `nothing-${version}`;
 self.addEventListener('install', e => {
     e.waitUntil(
         caches.open(cacheName).then(
-                cache => {
-                    return cache.addAll([
-                        `/`,
-                        `/index.html`,
-                        '/index.js',    
-                    ]).then(() => self.skipWaiting());
-                }
-            );
+            cache => {
+                return cache.addAll([
+                    `/`,
+                    `/index.html`,
+                    '/index.js',
+                ]).then(() => self.skipWaiting());
+            }
+        )
     );
 });
 
@@ -22,9 +22,13 @@ self.addEventListener('activate', event => {
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.open(cacheName).then(
-                cache => cache.match(event.request, { ignoreSearch: true });
-            ).then(
-                response => {return response || fetch(event.request)} 
-            );
+            cache => cache.match(event.request, {
+                ignoreSearch: true
+            })
+        ).then(
+            response => {
+                return response || fetch(event.request)
+            }
+        )
     );
 });
